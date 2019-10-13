b9ffa34f39db1a723bcbb7224308c377ba1262ac
Aditya Medhe <adityamedhe3@gmail.com>
2019-10-13 10:10:29 +0530

    Added top-level scripts to make management easy

--- packages/test-server/index.ts
+++ packages/test-server/index.ts
@@ -2,7 +2,7 @@ import { EntityStoreWatchManagerMongo } from 'entitystore-watch-manager-mongo';
 import { ClientManagerSocketIO } from 'client-manager-socketio';
 import { connect } from 'mongodb';
 import { EntityStoreWatchManager } from 'live-components-api';
-import { Server, createServer } from 'http';
+import { Server } from 'http';
 import express from 'express';
 
 const app = express();
@@ -10,9 +10,6 @@ const server = new Server(app);
 server.listen(5000);
 
 app.use(express.static('./static'));
-app.get('/', (req, res) => {
-  res.send('Haa bhai');
-});
 
 (async () => {
   const client = await connect('mongodb://localhost:27017');
@@ -20,6 +17,7 @@ app.get('/', (req, res) => {
   const entityWatchManagerMongo: EntityStoreWatchManager = new EntityStoreWatchManagerMongo(
     restaurant,
   );
+
   const clientManager = new ClientManagerSocketIO(
     entityWatchManagerMongo,
     server,
